e7dd47750187a284e910be933c4496610dc9c270
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-02-12 12:17:36 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -8,6 +9,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(routes)
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+})
 app.listen(PORT, () => {
     console.log('Running on http://localhost:3001')
 })
