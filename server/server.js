9303b0f980fc8a0197fcbb03a72ba14cdc17f8c6
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-02-12 12:24:42 -0500

    updated server file

--- server/server.js
+++ server/server.js
@@ -7,11 +7,16 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
 app.use(routes)
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 })
+
 app.listen(PORT, () => {
-    console.log('Running on http://localhost:3001')
+    console.log(`Running on http://localhost:${PORT}`)
 })
